791a550d1963e99fedb8f6b5df138fdbaeaa316f
Est Chairat <est.kungzc@gmail.com>
2021-05-20 22:00:56 +0700

    feat: :sparkles: add webscraping

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # webscrape-cypress
+
+## Description
+
+-   To extract data from http://air4thai.pcd.go.th/webV2/history/ without using API
+
+## Requirements
+
+-   NodeJS
+
+## Usage
+
+1. Clone this repository, `git clone https://github.com/estkungzc/webscrape-cypress.git`
+2. Then, `npm install`
+3. To start cypress, `npm run cypress`
+4. Start collecting the data, run `scraper.spec.js` in cypress
+
+## Others
+
+## Issues
+
+-   It took a long time to retrieve data each page, because of wating time each step to prevent errors of operation
+
+## Others
+
+To specific station, you can replace `stations` then dump the station id instead \
+Before
+
+```js
+    26	stations.forEach(async (station, index) => {
+    27		cy.wait(500);
+    28		cy.get('select#station_name').select(station);
+```
+
+After; where `02t` is a station id
+
+```js
+    26	['02t'].forEach(async (station, index) => {
+    27		cy.wait(500);
+    28		cy.get('select#station_name').select(station);
+```
+
+## Project Structure
+
+    .
+    ├── cypress
+    │   ├── fixtures
+    │   ├── integration
+    │   │   └── scraper.spec.js  // extract data to json
+    │   ├── plugins
+    │   └── support
+    ├── ...
+    ├── package.json
+    └── README.md
+    ````
+
+```
+
+```
---
+++ cypress/integration/scraper.spec.js
@@ -0,0 +1,86 @@
+/// <reference types="cypress" />
+
+import promisify from 'cypress-promise';
+const { _, $ } = Cypress;
+
+describe('Collecting Data', () => {
+	before(() => {
+		cy.visit('http://air4thai.pcd.go.th/webV2/history');
+	});
+
+	it('Creating the json data', async () => {
+		const params = await promisify(
+			cy
+				.get('select#parameter_name option')
+				.then(($el) => _.slice($el).map((x) => x.value))
+		);
+
+		await promisify(cy.get('select#parameter_name').select(params));
+
+		let stations = await promisify(
+			cy
+				.get('select#station_name option')
+				.then(($el) => _.slice($el).map((x) => x.value))
+		);
+
+		stations.forEach(async (station, index) => {
+			cy.wait(500);
+			cy.get('select#station_name').select(station);
+
+			// Click this button to show data table
+			cy.contains('ตาราง Table').click();
+
+			const pages = await promisify(
+				cy.get('ul.pagination li').then(($li) => {
+					const pagesElement = $li[$li.length - 2];
+					const pages = parseInt(pagesElement.innerText);
+					return pages;
+				})
+			);
+
+			const cols = await promisify(
+				cy.get('table#table1 thead tr').then(($tr) => {
+					const rowElement = $tr.get(0);
+					const cells = rowElement.cells;
+					const cols = _.slice(cells).map((c) => c.innerText);
+					return cols;
+				})
+			);
+			cy.wait(500);
+
+			let results = [];
+			for (let index = 0; index < pages; index++) {
+				// const element = array[index];
+
+				const data = await promisify(
+					cy.get('table#table1 tbody tr').then(($tr) => {
+						const rowsElement = _.slice($tr);
+						let r = [];
+						rowsElement.forEach((el) => {
+							const cells = el.cells;
+							const cols = _.slice(cells).map((c) => c.innerText);
+							r.push(cols);
+						});
+						return r;
+					})
+				);
+
+				data.forEach((row) => {
+					let obj = {};
+
+					row.forEach((el, idx) => {
+						obj[cols[idx]] = el;
+					});
+					results.push(obj);
+				});
+
+				cy.get('li#table1_next').contains('Next').click();
+				cy.wait(500);
+			}
+			console.log(results);
+
+			cy.writeFile(`data/${stations[index]}.json`, results);
+			// cy.wait(300);
+		});
+	});
+});
